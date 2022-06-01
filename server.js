8e4b84236d18f289f75e72d444cfae4f956f2a8f
Kuldeep Jambhulkar <jambku@2020spaces.com>
2022-06-01 16:34:14 +0530

    Code refactor

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
-
-const userData = require('./usersData');
+const Users = require('./Models/Users');
+const Tweets = require('./Models/Tweet');
 
 var currentLoggedInUser = '';
-
 app.use(express.json());
 app.set('view engine', 'ejs');
 
@@ -14,16 +12,6 @@ mongoose.connect('mongodb://localhost/TweetDB', { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => { console.error(error.message) });
 db.once('open', () => { console.log("Connected!") }); 
-
-// Routers
-// const usersRouter = require('./routes/usersRoutes');
-// const tweetsRouter = require('./routes/tweetsRoutes');
-
-// app.use('/users', usersRouter);
-// app.use('/tweets', tweetsRouter);
-
-
-const Users = require('./Models/Users');
 
 app.get('/', (req, res) =>{
     res.render('pages/index')
@@ -37,8 +25,7 @@ app.get('/login', (req, res) => {
     res.render('pages/login');
 })
 
-
-// for all users
+// Get all users
 app.get('/Users',
 	async(req, res) => {
 		try {
@@ -53,117 +40,100 @@ app.get('/Users',
 		}
 	});
 
-// For single user 
-app.get('/User/:username',
-	async(req, res) => {
-		try {
-			const resultData = await Users.find({ UserName: {$eq: req.params.username} });
-            if(resultData[0]){
-                res.json({
-                    status: 'ok',
-                    data: resultData
-                });
-                currentLoggedInUser = resultData[0];
-            }else{
-                res.json({
-                    status: 'error'
-                });
-
+    // Get  user by username (used for login verification)
+    app.get('/User/:username',
+        async(req, res) => {
+            try {
+                const resultData = await Users.find({ UserName: {$eq: req.params.username} });
+                if(resultData[0]){
+                    res.json({
+                        status: 'ok',
+                        data: resultData
+                    });
+                    currentLoggedInUser = resultData[0];
+                }else{
+                    res.json({status: 'error'});
+                }
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
             }
-		} catch (error) {
-			console.log(error.message);
-			res.json({status: "error", error});
-		}
+    });
+
+    // Create user (used in signUp page)
+    app.post('/Users',
+        async(req, res) => {
+            const user = new Users({
+                Email: req.body.Email,
+                Password: req.body.Password,
+                DisplayName: req.body.DisplayName,
+                UserName: req.body.UserName,
+                Bio: req.body.Bio
+            });
+
+            try {
+                const newUser = await user.save();
+                res.json(newUser);
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
+            }
+    });
+
+    // Get all tweets (useed in /tweets page)
+    app.get('/Tweets',
+        async(req, res) => {
+            try {
+                const tweetsAndUserObj = {};
+                const resultData = await Tweets.find();
+                tweetsAndUserObj.resultData = resultData;
+                tweetsAndUserObj.UserName = "currentLoggedInUser";
+                res.render('pages/tweets', {tweetsAndUserObj});
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
+            }
+    });
+
+    // Create new tweet
+    app.post('/Tweets',
+        async(req, res) => {
+            const tweet = new Tweets({
+                author: currentLoggedInUser,
+                textContent: req.body.textContent,
+                likes: 0
+            });
+
+            try {
+                const newTweet = await tweet.save();
+                res.json(newTweet);
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
+            }
 	});
 
-
-app.post('/Users',
-	async(req, res) => {
-		const user = new Users({
-            Email: req.body.Email,
-            Password: req.body.Password,
-            DisplayName: req.body.DisplayName,
-			UserName: req.body.UserName,
-			Bio: req.body.Bio
-		});
-
-		try {
-			const newUser = await user.save();
-			res.json(newUser);
-		} catch (error) {
-            console.log(error.message);
-			res.json({status: "error", error});
-		}
+    // Update like count by tweetID
+    app.patch('/Tweets/liked/:id',
+        async(req, res) => {
+            try {
+                const targetTweet = await Tweets.findById(req.params.id);
+                targetTweet.likes = parseInt(targetTweet.likes) + 1;
+                const oneTweet = await targetTweet.save();
+                res.json(oneTweet);
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
+            }
 	});
 
-
-const Tweets = require('./Models/Tweet');
-const req = require('express/lib/request');
-const { status } = require('express/lib/response');
-app.get('/Tweets',
-	async(req, res) => {
-
-		try {
-            const tweetsAndUserObj = {};
-
-			const resultData = await Tweets.find();
-            
-            tweetsAndUserObj.resultData = resultData;
-            tweetsAndUserObj.UserName = "currentLoggedInUser";
-
-            res.render('pages/tweets', {tweetsAndUserObj});
-
-		} catch (error) {
-			console.log(error.message);
-			res.json({status: "error", error});
-		}
-	});
-
-app.post('/Tweets',
-	async(req, res) => {
-
-        const currentAuthor = userData[Math.floor(Math.random()*userData.length)];
-
-		const tweet = new Tweets({
-			author: currentLoggedInUser,
-			textContent: req.body.textContent,
-			likes: 0
-		});
-
-		try {
-			const newTweet = await tweet.save();
-			res.json(newTweet);
-		} catch (error) {
-			console.log(error.message);
-			res.json({status: "error", error});
-		}
-	});
-
-app.patch('/Tweets/liked/:id',
-	async(req, res) => {
-		try {
-			
-             const targetTweet = await Tweets.findById(req.params.id);
-             targetTweet.likes = parseInt(targetTweet.likes) + 1;
-             const oneTweet = await targetTweet.save();
-
-            // const oneTweet = await Tweets.updateOne({ _id: req.params.id }, { $set: { likes:  Math.floor(Math.random()*9999) } });
-            res.json(oneTweet);
-		} catch (error) {
-			console.log(error.message);
-			res.json({status: "error", error});
-		}
-	});
-
+    // Edit tweetContent by tweetID
     app.put('/Tweets/edit/:id',
 	async(req, res) => {
 		try {
-			
-             const targetTweet = await Tweets.findById(req.params.id);
-             targetTweet.textContent = req.body.textContent;
-             const oneTweet = await targetTweet.save();
-
-            // const oneTweet = await Tweets.updateOne({ _id: req.params.id }, { $set: { likes:  Math.floor(Math.random()*9999) } });
+            const targetTweet = await Tweets.findById(req.params.id);
+            targetTweet.textContent = req.body.textContent;
+            const oneTweet = await targetTweet.save();
             res.json(oneTweet);
 		} catch (error) {
 			console.log(error.message);
@@ -171,26 +141,18 @@ app.patch('/Tweets/liked/:id',
 		}
 	});
 
-
-app.delete('/Tweets/Delete/:id',
-	async (req, res) => {
-		try {
-
-			const targetTweet = await Tweets.findById(req.params.id);
-			//targetTweet.likes = parseInt(targetTweet.likes) + 1;
-			//const oneTweet = await targetTweet.save();
-
-			 await Tweets.deleteOne({ _id: req.params.id });
-
-
-			res.json(targetTweet);
-		} catch (error) {
-			console.log(error.message);
-			res.json({status: "error", error});
-		}
+    // Delete tweet by tweetID
+    app.delete('/Tweets/Delete/:id',
+        async (req, res) => {
+            try {
+                const targetTweet = await Tweets.findById(req.params.id);
+                await Tweets.deleteOne({ _id: req.params.id });
+                res.json(targetTweet);
+            } catch (error) {
+                console.log(error.message);
+                res.json({status: "error", error});
+            }
     });
-
-
 
 app.listen(3000,
 	() =>
